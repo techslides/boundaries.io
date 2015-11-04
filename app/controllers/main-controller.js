437618d2aa3fec6d62fcdb11bdf5bfdf1242e89c
Josh Bielick <jbielick@gmail.com>
2015-11-04 00:12:13 -0500

    add readme file markdowned to about page, css

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -1,7 +1,13 @@
 var ApplicationController = require('./application-controller');
+var fs = require('fs');
+var marked = require('marked');
 
 var MainController = ApplicationController.extend({
-  about: function*() {},
+  about: function*() {
+    var markdown = fs.readFileSync(process.cwd() + '/README.md').toString();
+    var html = marked(markdown);
+    this.set('readme', html);
+  },
   show: function*() {
     yield this.respondTo({
       html: function*() {
