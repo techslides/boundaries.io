55894700b5434d16500c8cb633b1c2fe95b9b83b
Josh Bielick <jbielick@gmail.com>
2015-11-29 00:40:24 -0500

    add koa-compress to gzip responses
    large geo payloads were upwards of 800kb, using koa-compress will gzip
    these responses and save >50% of the bandwidth in some cases.
    
    bumped kona version for the middleware hook (appending middleware)

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var Kona = require('kona');
 var app = new Kona({root: __dirname});
+var compress = require('koa-compress');
+
+// add some middleware
+app.on('hook:middleware', function* () {
+  this.use(compress());
+});
 
 app.initialize().on('ready', function() {
   this.listen();
