e2f8b0d5d8f386ee16538ab6383aeeb3b629bdb3
Josh Bielick <jbielick@gmail.com>
2015-11-29 17:17:16 -0500

    update kona-mongo and kona versions for fixes

--- config/application.js
+++ config/application.js
@@ -5,16 +5,7 @@ module.exports = function(config) {
 
   config.gaTrackingId = 'UA-11539854-15';
 
-  config.mongo = {
-    host: process.env.DB_HOST,
-    name: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    auth: {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
-    },
-    collections: ['counties', 'postalcodes', 'states', 'cities', 'places']
-  };
+  config.mongo = process.env.DB_URL;
 
   config.port = 3334;
 
